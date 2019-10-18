3e50f99495995f4401ae13b2154aebe06167ad9e
buigialam19 <56628835+buigialam19@users.noreply.github.com>
2019-10-18 20:54:03 +0700

    web

--- wp-content/plugins/videoapi.io/read-sub.php
+++
@@ -1,8 +0,0 @@
-<?php 
-header('Content-Type: text/x-subrip; charset=utf-8');
-if(isset($_GET['file']) && $_GET['file']){
-    $file = $_GET['file'];
-    $content = @file_get_contents($file);
-    echo $content;
-}
-?>
--- wp-content/themes/vesinh/sidebar.php
+++
@@ -1,9 +0,0 @@
-<!-- sidebar -->
-<aside class="sidebar" role="complementary">
-
-	<div class="sidebar-widget">
-		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
-	</div>
-
-</aside>
-<!-- /sidebar -->
